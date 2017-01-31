cf387f353e06a900f1ba6474994ba1c01616f6e6
zeinabd67@gmail.com <zeinabd67@gmail.com>
2017-01-31 15:46:00 +0330

    Add project files.

---
+++ MyApplication_C_Sharp_1/Infrastructure/BaseForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace MyApplication_C_Sharp_1.Infrastructure
+{
+    public partial class BaseForm : System.Windows.Forms.Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyApplication_C_Sharp_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Data.Entity;
+
+
+namespace MyApplication_C_Sharp_1
+{
+    static class Program
+    {
+        static Program()
+        {
+        }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [System.STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+
+            //**************************************************
+            LoginForm frmStartup = new LoginForm();
+
+            System.Windows.Forms.Application.Run(frmStartup);
+
+            if(frmStartup!=null)
+            {
+                if(frmStartup.IsDisposed==false)
+                {
+                    frmStartup.Dispose();
+                }
+                frmStartup = null;
+            }
+        }
+    }
+}
